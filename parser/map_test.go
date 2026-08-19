e9e37a7133a9096cbd8c06940b9146e3f72536c5
Edd <47914740+esweby@users.noreply.github.com>
2026-08-19 14:31:31 +0100

    Added in another test case

--- parser/map_test.go
+++ parser/map_test.go
@@ -284,3 +284,85 @@ map[string]map[string]string{
 		
 	}
 }
+
+func TestMapDeclaration(t *testing.T) {
+	tests := []struct{
+		input string
+		expectedIdentifier string
+		expectedKeyType types.Type
+		expectedValueType types.Type
+		expectedKey string
+		expectedValue string
+	}{
+		{`a: map[int32]string := map[int32]string{ 10: "ten", }`, "a", types.Int32Type, types.StringType, "10", "ten"},
+	}
+
+	for i, tt := range tests {
+		l := lexer.New(tt.input)
+		p := New(l)
+
+		program := p.ParseProgram()
+		requireNoParserErrors(t, p)
+
+		stmt, ok := program.Statements[0].(*ast.DeclareStatement)
+		if !ok {
+			t.Fatalf("test %d: program.Statements[0] not ast.DeclareStatement. Got=%T", i, program.Statements[0])
+		}
+
+		ident := stmt.Name.Value
+		if ident != tt.expectedIdentifier {
+			t.Fatalf("test %d: stmt.Name.Value not=%s. Got=%s", i, tt.expectedIdentifier, ident)
+		}
+
+		mapExp, ok := stmt.Value.(*ast.MapLiteral)
+		if !ok {
+			t.Fatalf("test %d: exp not *ast.MapLiteral. Got=%T", i, stmt.Value)
+		}
+
+		mapTypes, ok := mapExp.Type.(*types.Map)
+		if !ok {
+			t.Fatalf("test %d: exp.Type not *types.Map. Got=%T", i, mapExp.Type)
+		}
+
+		if !types.IsTypesEqual(tt.expectedKeyType, mapTypes.Key) {
+			t.Errorf(
+				"test %d: expected key to be type: %s, got: %s", 
+				i, 
+				tt.expectedKeyType.Name(),
+				mapTypes.Key.Name(),
+			)
+		}
+
+		if !types.IsTypesEqual(tt.expectedValueType, mapTypes.Value) {
+			t.Errorf(
+				"test %d: expected value to be type: %s, got: %s", 
+				i, 
+				tt.expectedValueType.Name(),
+				mapTypes.Value.Name(),
+			)
+		}
+
+		if len(mapExp.Pairs) != 1 {
+			t.Fatalf("test %d: expected 1 pairs, got %d", i, len(mapExp.Pairs))
+		}
+
+		pair := mapExp.Pairs[0]
+		if pair.Key.String() != tt.expectedKey {
+			t.Errorf(
+				"test %d: expected key to be: %s, got: %s", 
+				i, 
+				tt.expectedKey,
+				pair.Key.String(),
+			)
+		}
+
+		if pair.Value.String() != tt.expectedValue {
+			t.Errorf(
+				"test %d: expected value to be: %s, got: %s", 
+				i, 
+				tt.expectedValue,
+				pair.Value.String(),
+			)
+		}
+	}
+}
